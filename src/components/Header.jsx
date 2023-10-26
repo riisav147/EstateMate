63ce836b15a63848de669880a9cceb30ba9a33c5
Rishav Raj <rishavraj471@gmail.com>
2023-10-26 17:54:31 +0530

    correct some minor issue that is spell of center

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,14 @@ import {Link} from 'react-router-dom'
 export default function Header() {
   return (
     <header className='bg-slate-200 shadow-md'>
-        <div className='flex justify-between items-centre max-w-6xl mx-auto p-3'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to='/'>
-            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap items-center'>
+            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
                 <span className='text-slate-500'>Estate</span>
                 <span className='text-slate-700'>Mate</span>
             </h1>
             </Link>
-            <form className='bg-slate-100 p-3 rounded-lg flex items-centre'>
+            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
                 <input 
                     type='text'
                     placeholder='Search...' 
