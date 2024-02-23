abf47448f0d8fa2864468a2d9c9d3c17589e4ba3
Rishav Raj <rishavraj471@gmail.com>
2024-02-23 17:22:44 +0530

    complete signin page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -27,12 +27,12 @@ export const signin = async (req, res, next) => {
         if(!validUser)return next(errorHandler(404,'User not found!'));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if(!validPassword)return next(errorHandler(401,'Wrong credential'));
-        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
         const{password:pass, ...rest} = validUser._doc;
         res
-            .cookies('access_token',token,{httpOnly:true})
+            .cookie('access_token',token,{httpOnly:true})
             .status(200)
-            .json(validUser);
+            .json(rest);
     }catch(error){
         next(error);
     }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,11 @@
 import {useState} from 'react'
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   const handleChange = (e)=>{
     setFormData({
       ...formData,
@@ -13,26 +14,32 @@ export default function SignUp() {
   };
   const handleSubmit = async(e) => {
     e.preventDefault();
-    setLoading(true);
-    const res = await fetch('/api/auth/signup',{
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json();
-    if(data.success === false){
+    try{
+      setLoading(true);
+      const res = await fetch('/api/auth/signup',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+        const data = await res.json();
+        console.log(data);
+        if(data.success === false){
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
       setLoading(false);
-      setError(data.message);
-      
-      return;
+      setError(null);
+      navigate('/sign-in');
+    }catch(error){
+      setLoading(false);
+      setError(error.message);
     }
-
-    setLoading(false);
-    console.log(data);
   };
-  console.log(formData);
+  
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -50,6 +57,7 @@ export default function SignUp() {
           <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
