e7d1851780a8227e32389df23a643982e30c91db
Rishav Raj <rishavraj471@gmail.com>
2024-02-27 01:51:19 +0530

    complete profile page ui

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,8 +1,9 @@
-import { UseSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Outlet,Navigate } from "react-router-dom";
 
 export default function PrivateRoute() {
-  return (
-    <div>PrivateRoute</div>
-  )
+    const{currentUser} = useSelector((state)=>state.user);
+  return currentUser?<Outlet/>:<Navigate to='/sign-in'/>
+    
+  
 }
