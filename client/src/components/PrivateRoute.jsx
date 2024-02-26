c2834cdd0a0d9d2491290f1f085a073d1018ea53
Rishav Raj <rishavraj471@gmail.com>
2024-02-26 18:20:57 +0530

    update the header and make the profile page private

---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,8 @@
+import { UseSelector } from "react-redux";
+import { Outlet,Navigate } from "react-router-dom";
+
+export default function PrivateRoute() {
+  return (
+    <div>PrivateRoute</div>
+  )
+}
